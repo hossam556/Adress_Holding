33c041be0b619e8718f5a0f9f5d3b6a927b29f0e
hossam <hossam.salem667@gmail.com>
2022-02-15 00:18:40 +0200

    sidebar

---
+++ components/Sidebar/Section.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Section = ({name , Icon , index}) => {
+  return (
+ <li className='mt-2'>
+    <a className={`flex items-center px-4 py-2 text-gray-700 hover:bg-gray-200 rounded-md ${index===1 && 'bg-gray-200'}`} href="#">
+        <Icon className="w-6 h-6 mr-5" />
+        <span className="font-medium">{name}</span>
+    </a>
+  </li>
+  )
+}
+
+export default Section
