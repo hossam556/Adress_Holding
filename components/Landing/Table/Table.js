c0de41aabc07d8d35fd81f391c3b03b92c6ced5b
hossam <hossam.salem667@gmail.com>
2022-02-16 03:34:26 +0200

    side

--- components/Landing/Table/Table.js
+++ components/Landing/Table/Table.js
@@ -1,7 +1,6 @@
 import React from 'react'
 
 const Table = ({newUsers}) => {
-  console.log(newUsers)
   
   const addCol = (e) => {
     let elements = document.querySelectorAll(`#${e.target.value}`);
